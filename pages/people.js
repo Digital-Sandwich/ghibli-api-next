d956249e315b33d30b19d4881c98fe5169a59a12
James Coombs <jcoombs@jcoombs-p-md01.putnaminv.com>
2017-04-24 14:24:15 -0400

    Write people page in existing page style.

--- pages/people.js
+++ pages/people.js
@@ -4,47 +4,93 @@ import PageHead from '../components/head';
 import Link from 'next/link';
 import Nav from '../components/nav';
 
-export default class extends React.Component {
+class InfoCard extends React.Component {
+  constructor() {
+    super();
+  }
+  render(){
+	const infoCardStyle = {
+		postiton: 'fixed',
+		top: '0',
+		width: '100vw',
+		listStyleType: 'none',
+		backgroundColor: '#a5d6a7',
+		border: '1px solid #F5F5F6',
+		textAlign: 'center',
+		verticalAlign: 'middle',
+		lineHeight: '1.7em',
+		borderRadius: '25px',
+		height: 'auto',
+	};
+    return(
+		<div className='infoCard' style={infoCardStyle}>
+			<li><b>Name:</b> {this.props.people[this.props.currentPersonId].name}</li>
+			<li><b>Gender:</b> {this.props.people[this.props.currentPersonId].gender}</li>
+			<li><b>Age:</b> {this.props.people[this.props.currentPersonId].age}</li>
+			<li><b>Eye Color:</b> {this.props.people[this.props.currentPersonId].eye_color}</li>
+			<li><b>Hair Color:</b> {this.props.people[this.props.currentPersonId].hair_color}</li>
+			<li><b>Films:</b> {this.props.people[this.props.currentPersonId].films}</li>
+			<li><b>Species:</b> {this.props.people[this.props.currentPersonId].species}</li>
+		</div>
+    )
+  }
+}
+export default class People extends React.Component {
+	constructor() {
+    	super();
+    	this.state = {
+      		showInfo: false,
+      		currentPersonId: null,
+    	};
+ 	}
+	onClick(i){
+    	this.setState({showInfo: !this.state.showInfo});
+		this.state.currentPersonId = i;
+	}
 	static async getInitialProps () {
 	    const peopleRes = await axios.get('https://ghibliapi.herokuapp.com/people');
-	    return {people: peopleRes.data,};
-	  }
-      render() {
-	      	const tableStyle = {
-		  		backgroundColor: '#E1E2E1',
-		  		border: '1px solid #F5F5F6',
-		  		textAlign: 'center',
-	      	 };
-	      	 const divPosition = {
-		      	marginLeft: '0 auto',
-		      	marginRight: '0 auto',
-		      	width: '200px',
-	      	  };
+	    return {people: peopleRes.data};
+	}
+	render() {
+		const tableStyle = {
+			backgroundColor: '#E1E2E1',
+			border: '1px solid #F5F5F6',
+			textAlign: 'center',
+		};
+		const peopleListStyle = {
+			marginLeft: '0 auto',
+			marginRight: '0 auto',
+			width: '200px',
+			position: 'absolute,'
+		};
+		const peopleItemStyle = {
+			top: '0px',
+			width: '100vw',
+			backgroundColor: '#E1E2E1',
+			border: '1px solid #F5F5F6',
+			textAlign: 'center',
+			verticalAlign: 'middle',
+			lineHeight: '30px',
+			borderRadius: '25px',
+			height: '5em'
+		};
         return (
         	<div>
         	<PageHead />
         	<Nav />
-        	<div className='people-list' style={tableStyle, divPosition}>
-        		<table className='data-table' style={divPosition, tableStyle}>
-        	    	<thead>
-        	          <tr>
-        	              <th className='data-table-head'>Name</th>
-        	          </tr>
-        	        </thead>
-        	        <tbody>
-        	          {
-        	            this.props.people.map( (person, i) => (
-        	            	<tr key={i}>
-        	                    <td><Link href='/people'><a>{ person.name }</a></Link></td>
-        	                </tr>
-        	              ))
-        	          }
-        	       </tbody>
-        	      </table>
-        	      </div>
+        		<div className='people-list' style={tableStyle, peopleListStyle}>
+					{
+						this.state.showInfo && <InfoCard people={this.props.people} currentPersonId={this.state.currentPersonId} />
+					}
+					{
+						this.props.people.map((people, i) => (
+							<div className='people-item' style={peopleItemStyle} key={i} onClick={this.onClick.bind(this, i)}>
+								<h4>{people.name}</h4>
+							</div>
+						))
+					}
+        	    </div>
             </div>
         )
-      }
+    }
  }
-
-
